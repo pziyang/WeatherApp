73815b8013342f09e64b37bd90f64d75ea305074
Ziyang Pang <pziyang@umich.edu>
2016-11-18 00:05:24 -0500

    Bugfix: Correct parsing of current observations

--- Weather.cpp
+++ Weather.cpp
@@ -57,6 +57,7 @@ bool Weather::ParseXml() {
 	//load string into XML DOM
 	pugi::xml_parse_result result = xmldoc_.load(body_.c_str());
 
+	//check if there are errors
 	if (result) {
 		std::cout << "XML parsed without errors" << std::endl;
 	}
@@ -70,33 +71,35 @@ bool Weather::ParseXml() {
 
 bool Weather::GetCurrentWeather() {
 
-    //this is the top level node
-    pugi::xml_node rootnode = xmldoc_.child("dwml");
+	//this is the top level node
+	pugi::xml_node rootnode = xmldoc_.child("dwml");
 
-    //search all child for current observations
-    for (pugi::xml_node it = rootnode.first_child(); it; it = it.next_sibling()) {
+	//search all child for current observations
+	for (pugi::xml_node it = rootnode.first_child(); it; it = it.next_sibling()) {
 
-        if (it.attribute("type").as_string() == "current observations") {
+		if (strcmp(it.attribute("type").as_string(),"current observations") == 0) {
+						
+			//read current temperature
+			ReadXmlChildValue(weather_data_.temperature, it.child("parameters").child("temperature").child("value"));
 
-            //read current temperature
-            ReadXmlChildValue(weather_data_.temperature, it.child("parameters").child("temperature"));
+			//read current weather conditions
+			weather_data_.weather_conditions = it.child("parameters")
+				.child("weather").child("weather-conditions")
+				.attribute("weather-summary").as_string();
 
-            //read current  weather conditions
-            weather_data_.weather_conditions = it.child("parameters")
-                    .child("weather").child("weather-conditions")
-                    .attribute("weather-summary").as_string();
+			//read sustained wind speed
+			ReadXmlChildValue(weather_data_.wind_speed, it.child("parameters")
+				.child("wind-speed").next_sibling().child("value"));
 
-            //read wind speed
-            ReadXmlChildValue(weather_data_.wind_speed, it.child("parameters")
-                    .child("wind-speed").child("value"));
-        }
-    }
+			break;
+		}
+	}
 
-    return true;
+	return true;
 }
 
 bool Weather::PrintCurrentWeather() {
-    std::cout << weather_data_.temperature << std::endl
-              << weather_data_.weather_conditions << std::endl
-              << weather_data_.wind_speed << std::endl;
+	std::cout << weather_data_.temperature << std::endl
+		<< weather_data_.weather_conditions << std::endl
+		<< weather_data_.wind_speed << std::endl;
 }
